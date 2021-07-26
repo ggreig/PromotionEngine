c992f9c805a3881ba5ee744bea3b04faca29a372
Dr. Gavin T.D. Greig <ggreig@ggreig.com>
2021-07-26 22:23:13 +0100

    Add Cart and ensure SKUs can be added successfully.

---
+++ PromotionEngine/Cart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class Cart
+    {
+        public Dictionary<StockKeepingUnit, uint> Contents { get; private set; }
+            = new Dictionary<StockKeepingUnit, uint>();
+
+        public Cart()
+        {
+        }
+
+        /// <remarks>
+        /// Return "this" so that SKUs can be added to the cart fluently.
+        /// </remarks>
+        public Cart Add(StockKeepingUnit inStockKeepingUnit)
+        {
+            if (Contents.ContainsKey(inStockKeepingUnit))
+            {
+                Contents[inStockKeepingUnit]++;
+            }
+            else
+            {
+                Contents.Add(inStockKeepingUnit, 1);
+            }
+
+            return this;
+        }
+    }
+}
---
+++ PromotionEngineUnitTest/CartTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace PromotionEngine.Tests
+{
+    [TestFixture]
+    public class CartTests
+    {
+        [Test]
+        public void AddSkuToCartSucceeds()
+        {
+            // Arrange
+            var theCart = new Cart();
+
+            const uint theUnitPrice = 20;
+            var anSku = new StockKeepingUnit('A', theUnitPrice);
+
+            // Act
+            theCart.Add(anSku);
+
+            // Assert
+            Assert.That(theCart.Contents[anSku], Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddTwoIdenticalSkusToCartSucceeds()
+        {
+            // Arrange
+            var theCart = new Cart();
+
+            const uint theUnitPrice = 20;
+            var anSku = new StockKeepingUnit('A', theUnitPrice);
+
+            // Act
+            theCart.Add(anSku).Add(anSku);
+
+            // Assert
+            Assert.That(theCart.Contents[anSku], Is.EqualTo(2));
+        }
+    }
+}
