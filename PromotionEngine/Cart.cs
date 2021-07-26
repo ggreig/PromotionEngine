84edee083c0f78d5f9fc9376eadd8be173a2b536
Dr. Gavin T.D. Greig <ggreig@ggreig.com>
2021-07-27 00:25:02 +0100

    Added price calculation with promotions to Cart, and tests for given scenarios and individual promotions.

--- PromotionEngine/Cart.cs
+++ PromotionEngine/Cart.cs
@@ -9,8 +9,12 @@ namespace PromotionEngine
 {
     public class Cart
     {
+        private uint myPromotionMatchCount;
+
         public Dictionary<StockKeepingUnit, uint> Contents { get; private set; }
             = new Dictionary<StockKeepingUnit, uint>();
+
+        private Dictionary<StockKeepingUnit, uint> StandardPriceContents { get; set; }
 
         public Cart()
         {
@@ -31,6 +35,72 @@ namespace PromotionEngine
             }
 
             return this;
+        }
+
+        public uint GetPrice(IPromotion inPromotion)
+        {
+            ApplyPromotion(inPromotion);
+
+            uint thePriceTotal = myPromotionMatchCount * inPromotion.GetPrice();
+
+            foreach (var (theStockKeepingUnit, theCount) in StandardPriceContents)
+            {
+                thePriceTotal += theStockKeepingUnit.UnitPrice * theCount;
+            }
+
+            return thePriceTotal ;
+        }
+
+        private void ApplyPromotion(IPromotion inPromotion)
+        {
+            StandardPriceContents = Contents;
+
+            myPromotionMatchCount = ApplyPatternMatches(inPromotion);
+        }
+
+        private uint ApplyPatternMatches(IPromotion inPromotion)
+        {
+            bool isPatternMatch = true;
+            uint theMatchCount = 0;
+
+            do
+            {
+                isPatternMatch = IsPatternMatchFound(inPromotion);
+
+                if (isPatternMatch)
+                {
+                    theMatchCount++;
+                    RemoveAMatch(inPromotion);
+                }
+            } while (isPatternMatch);
+
+            return theMatchCount;
+        }
+
+        private bool IsPatternMatchFound(IPromotion inPromotion)
+        {
+            bool isPatternMatch = true;
+
+            foreach (var _ in inPromotion.Pattern.Where(x => !SkuCountAvailable(x)))
+            {
+                isPatternMatch = false;
+            }
+
+            return isPatternMatch;
+        }
+
+        private void RemoveAMatch(IPromotion inPromotion)
+        {
+            foreach (var (anSku, thePatternSkuCount) in inPromotion.Pattern)
+            {
+                StandardPriceContents[anSku] -= thePatternSkuCount;
+            }
+        }
+
+        private bool SkuCountAvailable(KeyValuePair<StockKeepingUnit, uint> inSkuCount)
+        {
+            var (anSku, thePatternSkuCount) = inSkuCount;
+            return StandardPriceContents.ContainsKey(anSku) && StandardPriceContents[anSku] >= thePatternSkuCount;
         }
     }
 }
--- PromotionEngineUnitTest/CartTests.cs
+++ PromotionEngineUnitTest/CartTests.cs
@@ -1,10 +1,34 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace PromotionEngine.Tests
 {
     [TestFixture]
     public class CartTests
     {
+        private readonly StockKeepingUnit A = new StockKeepingUnit('A', 50);
+        private readonly StockKeepingUnit B = new StockKeepingUnit('B', 30);
+        private readonly StockKeepingUnit C = new StockKeepingUnit('C', 20);
+        private readonly StockKeepingUnit D = new StockKeepingUnit('D', 15);
+
+        private readonly IPromotion Promotion1;
+        private readonly IPromotion Promotion2;
+        private readonly IPromotion Promotion3;
+
+        public CartTests()
+        {
+            Promotion1 = new NForFixedPricePromotion(A, 3, 130);
+            Promotion2 = new NForFixedPricePromotion(B, 2, 45);
+
+            var thePromotion3Pattern = new Dictionary<StockKeepingUnit, uint>
+            {
+                {C, 1},
+                {D, 1}
+            };
+            Promotion3 = new CombinationForFixedPricePromotion(thePromotion3Pattern, 30);
+        }
+
+
         [Test]
         public void AddSkuToCartSucceeds()
         {
@@ -35,6 +59,147 @@ namespace PromotionEngine.Tests
 
             // Assert
             Assert.That(theCart.Contents[anSku], Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ScenarioA_WithPromotion1()
+        {
+            // Arrange
+            var theCart = new Cart();
+            theCart.Add(A).Add(B).Add(C);
+
+            // Act
+            uint theResult = theCart.GetPrice(Promotion1);
+
+            // Assert
+            Assert.That(theResult, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void ScenarioA_WithPromotion2()
+        {
+            // Arrange
+            var theCart = new Cart();
+            theCart.Add(A).Add(B).Add(C);
+
+            // Act
+            uint theResult = theCart.GetPrice(Promotion2);
+
+            // Assert
+            Assert.That(theResult, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void ScenarioA_WithPromotion3()
+        {
+            // Arrange
+            var theCart = new Cart();
+            theCart.Add(A).Add(B).Add(C);
+
+            // Act
+            uint theResult = theCart.GetPrice(Promotion3);
+
+            // Assert
+            Assert.That(theResult, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void ScenarioB_WithPromotion1()
+        {
+            // Arrange
+            var theCart = new Cart();
+            theCart.Add(A).Add(A).Add(A).Add(A).Add(A)
+                   .Add(B).Add(B).Add(B).Add(B).Add(B)
+                   .Add(C);
+
+            // Act
+            uint theResult = theCart.GetPrice(Promotion1);
+
+            // Assert
+            Assert.That(theResult, Is.EqualTo(400));
+        }
+
+        [Test]
+        public void ScenarioB_WithPromotion2()
+        {
+            // Arrange
+            var theCart = new Cart();
+            theCart.Add(A).Add(A).Add(A).Add(A).Add(A)
+                   .Add(B).Add(B).Add(B).Add(B).Add(B)
+                   .Add(C);
+
+            // Act
+            uint theResult = theCart.GetPrice(Promotion2);
+
+            // Assert
+            Assert.That(theResult, Is.EqualTo(390));
+        }
+
+        [Test]
+        public void ScenarioB_WithPromotion3()
+        {
+            // Arrange
+            var theCart = new Cart();
+            theCart.Add(A).Add(A).Add(A).Add(A).Add(A)
+                   .Add(B).Add(B).Add(B).Add(B).Add(B)
+                   .Add(C);
+
+            // Act
+            uint theResult = theCart.GetPrice(Promotion3);
+
+            // Assert
+            Assert.That(theResult, Is.EqualTo(420));
+        }
+
+        [Test]
+        public void ScenarioC_WithPromotion1()
+        {
+            // Arrange
+            var theCart = new Cart();
+            theCart.Add(A).Add(A).Add(A)
+                   .Add(B).Add(B).Add(B).Add(B).Add(B)
+                   .Add(C)
+                   .Add(D);
+
+            // Act
+            uint theResult = theCart.GetPrice(Promotion1);
+
+            // Assert
+            Assert.That(theResult, Is.EqualTo(315));
+        }
+
+        [Test]
+        public void ScenarioC_WithPromotion2()
+        {
+            // Arrange
+            var theCart = new Cart();
+            theCart.Add(A).Add(A).Add(A)
+                   .Add(B).Add(B).Add(B).Add(B).Add(B)
+                   .Add(C)
+                   .Add(D);
+
+            // Act
+            uint theResult = theCart.GetPrice(Promotion2);
+
+            // Assert
+            Assert.That(theResult, Is.EqualTo(305));
+        }
+
+        [Test]
+        public void ScenarioC_WithPromotion3()
+        {
+            // Arrange
+            var theCart = new Cart();
+            theCart.Add(A).Add(A).Add(A)
+                   .Add(B).Add(B).Add(B).Add(B).Add(B)
+                   .Add(C)
+                   .Add(D);
+
+            // Act
+            uint theResult = theCart.GetPrice(Promotion3);
+
+            // Assert
+            Assert.That(theResult, Is.EqualTo(330));
         }
     }
 }
