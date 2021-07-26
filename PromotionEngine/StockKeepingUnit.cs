963440f356e4c8ce40c15ed506e1929e8875fea9
Dr. Gavin T.D. Greig <ggreig@ggreig.com>
2021-07-26 20:38:58 +0100

    Initial class and trivial test included to ensure tests work.

---
+++ PromotionEngine/StockKeepingUnit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class StockKeepingUnit
+    {
+        public StockKeepingUnit(char inID)
+        {
+            Id = inID;
+        }
+
+        public char Id { get; private set; }
+    }
+}
---
+++ PromotionEngineUnitTest/StockKeepingUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace PromotionEngine.Tests
+{
+    [TestFixture]
+    public class StockKeepingUnitTests
+    {
+        [Test]
+        public void StockKeepingUnit_HasExpectedId()
+        {
+            // Arrange / Act
+            char theExpectedValue = 'a';
+            var theSku = new StockKeepingUnit(theExpectedValue);
+
+            // Act
+            char theResult = theSku.Id;
+
+            // Assert
+            Assert.That(theResult, Is.EqualTo(theExpectedValue));
+        }
+    }
+}
